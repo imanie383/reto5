15a127e61357b2495df60def5e392a3907b0ae0d
imanie383 <manuel@vauxoo.com>
2023-05-14 22:11:04 -0600

    [ADD] prosuga: module structure

---
+++ progusa/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License OEEL-1 (https://www.odoo.com/documentation/user/13.0/legal/licenses/licenses.html).
+
+{
+    'name': 'R5 - PROGUSA',
+    'summary': 'Reto 5',
+    'author': 'Vauxoo',
+    'website': 'http://www.vauxoo.com',
+    'license': 'OEEL-1',
+    'category': 'website',
+    'version': '14.0.1.0.1',
+    'depends': [
+        'website_sale',
+    ],
+    'data': [
+        'views/home.xml',
+    ],
+    'application': True,
+}
